26f0dfa4315b0d1339d71a4d9af7c9ce5ad4623a
Stacy Watts <swatts@pdx.edu>
2014-10-09 23:30:01 -0700

    take out comments on assembly parts 2 and 3

--- Assignment1/thread.c
+++ Assignment1/thread.c
@@ -31,27 +31,6 @@ int main()
   return 0;
 }
 
-
-// 2. Write an assembly routine to start a new thread, with the prototype:
-// void thread_start(struct thread_t * old, struct thread_t * new);
-//   a.  Push all callee-save registers (%rbx, %rbp, %r12-15) onto the current stack.
-//   b.  Save the current stack pointer (%rsp) in old's thread table entry.
-//   c.  Load the stack pointer from new's thread table entry into %rsp.
-//   d.  Jump to the initial function of new.
-
-void thread_start(struct thread_t * old, struct thread_t * new) {
-  return;
-}
-
-// 3. Write an assembly routine to switch between two threads, with the prototype:
-// void thread_switch(struct thread_t * old, struct thread_t * new);
-//   a.  Push all callee-save registers onto the current stack.
-//   b.  Save the current stack pointer in old's thread table entry.
-//   c.  Load the stack pointer from new's thread table entry.
-//   d.  Pop all callee-save registers from the new stack.
-//   e.  Return.
-
-
 // Your grade will primarily be determined by how well we think you understand 
 // the underlying concepts of each assignment, based not only on your code, but 
 // also how you've tested your code, and your write-ups.
