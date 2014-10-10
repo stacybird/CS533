39fd044653237c39b86610f04dce26b4fd1531d6
Stacy Watts <swatts@pdx.edu>
2014-10-09 19:37:58 -0700

    part 1 thread struct

--- Assignment1/thread.c
+++ Assignment1/thread.c
@@ -2,10 +2,32 @@
 // CS 533 - Fall 2014
 // Assignment 1
 //
-// develop a C data structure representing a thread
-// to develop and test the x86-64 assembly routines for starting threads and switching between them.
+// develop a C data structure representing a thread and to develop and test the 
+// x86-64 assembly routines for starting threads and switching between them.
 
-// 1.  Write a C data structure, struct thread_t, to represent a thread table entry (the data pertaining to a thread). At first, it should have at least a stack pointer (perhaps of type void*), and a pointer to an initial function (perhaps of type void(*initial_function)(void)).
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 1.  Write a C data structure, struct thread_t, to represent a thread table 
+//     entry (the data pertaining to a thread). At first, it should have at 
+//     least a stack pointer (perhaps of type void*), and a pointer to an initial 
+//     function (perhaps of type void(*initial_function)(void)).
+
+typedef struct {
+  void *sp; // stack pointer
+  void(*initial_function)(void);
+            // CPU Context
+            // Program counter
+            // Register State
+} thread_t;
+
+int main()
+{
+  thread_t myThread;
+
+  
+
 
 // 2. Write an assembly routine to start a new thread, with the prototype:
 // void thread_start(struct thread_t * old, struct thread_t * new);
@@ -22,3 +44,10 @@
 //   d.  Pop all callee-save registers from the new stack.
 //   e.  Return.
 
+
+// Your grade will primarily be determined by how well we think you understand 
+// the underlying concepts of each assignment, based not only on your code, but 
+// also how you've tested your code, and your write-ups.
+
+  return 0;
+}
