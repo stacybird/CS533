5c8f2a96f06bcfc63d3d3e3c6acefc4331b4db97
Stacy Watts <swatts@pdx.edu>
2014-10-21 08:35:11 -0700

    add test file Kendall suggested.  functions not implemented yet, will break

---
+++ Assignment2/test.c
@@ -0,0 +1,16 @@
+  #include "scheduler.h"
+
+
+  void foo(void * arg) {
+    char * str = (char*) arg;
+    /* do important stuff */
+  }
+
+
+  int main(void) {
+    scheduler_begin();
+    thread_fork(foo, (void*)"bar");
+    thread_fork(foo, (void*)"baz");
+    scheduler_end(); 
+  }
+
