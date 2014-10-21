e9919d616ca3c9233f540f28e2a256158a3a64b3
Stacy Watts <swatts@pdx.edu>
2014-10-21 08:27:44 -0700

    add initial args, add prototypes

--- Assignment2/scheduler.h
+++ Assignment2/scheduler.h
@@ -7,7 +7,14 @@ typedef enum {
 
 struct thread_t {
   void *sp; // stack pointer
-  void(*initial_function)(void);
+  void(*initial_function)(void*);
+  void * initial_arg;
   state_t state;
 };
+
+void scheduler_begin();
+void scheduler_end();
+void thread_fork(void(*target)(void*), void * arg);
+void thread_finish();
+void yield();
 
