3e4927692ac902c145e721db9df90b28890c44b0
Stacy Watts <swatts@pdx.edu>
2014-10-23 15:00:57 -0700

    add yield from assignment1 to scheduler

--- Assignment2/scheduler.c
+++ Assignment2/scheduler.c
@@ -1,5 +1,15 @@
+// definitions of our scheduler API functions.
+
 #include "scheduler.h"
 #include "queue.h"
 
+struct thread_t current_thread;
+struct thread_t stored_thread;
 
+void yield() {
+ struct thread_t temp = current_thread;
+ current_thread = stored_thread;
+ stored_thread = temp;
+ thread_switch(&stored_thread, &current_thread);
+}
 
