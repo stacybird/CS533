a85b2c3f1f141d2cecf260fcfc691479f2419501
Stacy Watts <swatts@pdx.edu>
2014-10-23 16:23:15 -0700

    add begin end fork empty functions to compile

--- Assignment2/scheduler.c
+++ Assignment2/scheduler.c
@@ -6,6 +6,15 @@
 struct thread_t current_thread;
 struct thread_t stored_thread;
 
+void scheduler_begin() {
+}
+
+void scheduler_end() {
+}
+
+void thread_fork(void(*target)(void*), void * arg) {
+}
+
 void yield() {
  struct thread_t temp = current_thread;
  current_thread = stored_thread;
