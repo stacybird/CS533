7098c22e70cb2a8e66ebb98dcd9b8370c85cbcc6
Stacy Watts <swatts@pdx.edu>
2014-10-23 13:23:37 -0700

    fix test.c indentation

--- Assignment2/test.c
+++ Assignment2/test.c
@@ -1,16 +1,16 @@
-  #include "scheduler.h"
+#include "scheduler.h"
 
 
-  void foo(void * arg) {
-    char * str = (char*) arg;
-    /* do important stuff */
-  }
+void foo(void * arg) {
+  char * str = (char*) arg;
+  /* do important stuff */
+}
 
 
-  int main(void) {
-    scheduler_begin();
-    thread_fork(foo, (void*)"bar");
-    thread_fork(foo, (void*)"baz");
-    scheduler_end(); 
-  }
+int main(void) {
+  scheduler_begin();
+  thread_fork(foo, (void*)"bar");
+  thread_fork(foo, (void*)"baz");
+  scheduler_end(); 
+}
 
