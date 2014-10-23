57a4b9d11e35b7018a2b6f3a4d7576b4246a8b0e
Stacy Watts <swatts@pdx.edu>
2014-10-23 16:23:47 -0700

    move prototypes into .h

--- Assignment2/scheduler.h
+++ Assignment2/scheduler.h
@@ -12,6 +12,9 @@ struct thread_t {
   state_t state;
 };
 
+void thread_start(struct thread_t * old, struct thread_t * new);
+void thread_switch(struct thread_t * old, struct thread_t * new);
+
 void scheduler_begin();
 void scheduler_end();
 void thread_fork(void(*target)(void*), void * arg);
