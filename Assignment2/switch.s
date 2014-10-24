8aa0a8376cbd5a421ed8df2791fa3f2665d9a03d
Stacy Watts <swatts@pdx.edu>
2014-10-24 00:06:07 -0700

    add part 4 comment to assembly file

--- Assignment2/switch.s
+++ Assignment2/switch.s
@@ -4,6 +4,17 @@
 
 .globl thread_start
 
+# 4. First, we'll need to make some slight modifications to our 
+#    thread_start assembly routine. Above, in the overview section, we 
+#    discussed how we might solve the problem of allowing a thread to 
+#    terminate gracefully.
+#    Implement the thread_finish function accordingly, then modify your 
+#    thread_start routine to push the address of thread_finish on the 
+#    bottom of the new thread's stack. This can be accomplished with 
+#    the instruction:    
+#               pushq $thread_finish
+#    We'll also want to pass our initial argument to the target function. #    Once you're done with %rdi, you can overwrite it with the value of 
+#    the initial argument.
 thread_start:
   pushq $thread_finish
   pushq %rbx           # callee-save
