c484c3065ceeaf9cd094c8ab768b737683137202
Stacy Watts <swatts@pdx.edu>
2014-11-09 15:09:49 -0800

    add assignment spec in comments

--- Assignment3/async.c
+++ Assignment3/async.c
@@ -8,4 +8,18 @@
 #include <unistd.h>
 #include <string.h>
 
+// Use the AIO interface to create an AIO control block and initiate an 
+// appropriate asynchronous read. Yield until the request is complete.
+//
+// read_wrap should have as close to the semantics of read as possible. That 
+// is, it should return the same value that read would have, and put the same 
+// result into buf that read would have.
+//
+// Furthermore, if the file is seekable, read_wrap should start reading from 
+// the current position in the file, and then seek to the appropriate position 
+// in the file, just as read would have. This is arguably the most difficult 
+// part of this assignment, since aio_read does not seek automatically.
 
+ssize_t read_wrap(int fd, void * buf, size_t count) {
+      // your code here!
+}
