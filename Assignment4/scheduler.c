066719b3a6bc05a7ceb51c83fbccc2a5ffe2c030
Stacy Watts <swatts@pdx.edu>
2014-12-10 14:38:19 -0800

    clean up comments to functions including whitespace

--- Assignment4/scheduler.c
+++ Assignment4/scheduler.c
@@ -1,6 +1,6 @@
 // Stacy Watts
 // CS 533 - Fall 2014
-// Assignment 2
+// Assignment 4
 
 // definitions of our scheduler API functions.
 
@@ -16,8 +16,7 @@ struct thread_t * current_thread = NULL;
 void thread_start(struct thread_t * old, struct thread_t * new);
 void thread_switch(struct thread_t * old, struct thread_t * new);
 
-// 5.  scheduler_begin should initialize and/or allocate any data 
-//     structures our scheduler will need.
+// initialization and allocation of scheduler
 void scheduler_begin() {
   ready_list = malloc(sizeof(struct queue));
   ready_list->head = NULL;
@@ -26,11 +25,8 @@ void scheduler_begin() {
   current_thread->state = RUNNING;
 }
 
-// 8. Finally, recall that we need a way to prevent the main thread from 
-//    terminating prematurely if there are other threads still running. A 
-//    potential solution is given in the overview above. Implement this 
-//    solution how you see fit in scheduler_end. You might find the 
-//    is_empty queue predicate useful.
+
+// prevent main thread from ending prematurely.
 void scheduler_end() {
   while (!is_empty(ready_list)) {
     yield();
@@ -38,39 +34,23 @@ void scheduler_end() {
 }
 
 
-// 6. Next, let's implement thread_fork. This function encapsulates 
-//    everything necessary to allocate a new thread and then jump to it. 
-//    thread_fork should:
-// a. Allocate a new thread table entry, and allocate its control stack.
-// b. Set the new thread's initial argument and initial function.
-// c. Set the current thread's state to READY and enqueue it on the ready 
-//    list.
-// d. Set the new thread's state to RUNNING.
-// e. Save a pointer to the current thread in a temporary variable, then 
-//    set the current thread to the new thread.
-// f. Call thread_start with the old current thread as old and the new 
-//    current thread as new.
+// + allocates the new thread table entry and stack
+// + sets the new thread argument and function
+// + sets original to ready and enquques
+// + sets new thread to running
+// + shuffle pointer to current thread, and call thread start
 void thread_fork(void(*target)(void*), void * arg) {
-// a. Allocate a new thread table entry, and allocate its control stack.
   struct thread_t * new_thread = malloc(sizeof(struct thread_t));
   void *stack_bottom = malloc(ALLOCATE);
   void *stack_top = stack_bottom + ALLOCATE;
   new_thread->sp = stack_top; 
-// b. Set the new thread's initial argument and initial function.
   new_thread->initial_function = target;
   new_thread->initial_arg = arg;
-// c. Set the current thread's state to READY and enqueue it on the ready 
-//    list.
   current_thread->state = READY;
   thread_enqueue(ready_list, current_thread);
-// d. Set the new thread's state to RUNNING.
   new_thread->state = RUNNING;
-// e. Save a pointer to the current thread in a temporary variable, then 
-//    set the current thread to the new thread.
   struct thread_t * temp_thread = current_thread;
   current_thread = new_thread;
-// f. Call thread_start with the old current thread as old and the new 
-//    current thread as new.
   thread_start(temp_thread, current_thread);
 }
 
@@ -78,42 +58,31 @@ void thread_fork(void(*target)(void*), void * arg) {
 // finish handler at the bottom of the stack which sets the current 
 // thread's state to DONE, then calls yield(); 
 void thread_finish() {
-  current_thread->state = DONE; // set state to done
-  yield();// call yield();
+  current_thread->state = DONE;
+  yield();
 }
 
 
-// 7. yield is very similar to thread_fork, with the main difference being 
-//    that it is pulling the next thread to run off of the ready list 
-//    instead of creating it. yield should:
-// a. If the current thread is not DONE, set its state to READY and 
-//    enqueue it on the ready list.
-// b. Dequeue the next thread from the ready list and set its state to 
-//    RUNNING.
-// c. Save a pointer to the current thread in a temporary variable, then 
-//    set the current thread to the next thread.
-// d. Call thread_switch with the old current thread as old and the new 
-//    current thread as new.
-// 4.1 Modify your yield routine to prevent a thread whose status is BLOCKED
-//     from enqueuing itself on the ready list.
+// + if current thread isn't blocked or done, put self on ready list.
+// + dequeue next thread, set running.
+// + shuffle pointer for current thread
+// + switch threads
+//
+// 4.1 Modify your yield routine to prevent a thread whose status is
+//     BLOCKED from enqueuing itself on the ready list.
 void yield() {
-// a. If the current thread is not DONE, set its state to READY and 
-//    enqueue it on the ready list.
-  if ((BLOCKED != current_thread->state) && (DONE != current_thread->state)) {
+  if (    (BLOCKED != current_thread->state) 
+       && (DONE != current_thread->state)) {
     current_thread->state = READY;
     thread_enqueue(ready_list, current_thread);
   }
-// b. Dequeue the next thread from the ready list and set its state to 
-//    RUNNING.
   struct thread_t * next_thread = thread_dequeue(ready_list);
   next_thread->state = RUNNING;
-// c. Save a pointer to the current thread in a temporary variable, then 
-//    set the current thread to the next thread.
   struct thread_t * temp_thread = current_thread;
   current_thread = next_thread;
-// d. Call thread_switch with the old current thread as old and the new 
   thread_switch(temp_thread, current_thread);
 }
+
 
 // 4.3 Code a data structure, struct mutex that represents your lock, 
 //     and three functions:
@@ -132,6 +101,7 @@ void mutex_init(struct mutex * lock) {
   blocked_list->tail = NULL;
 }
 
+
 void mutex_lock(struct mutex * lock) {
   if (lock->holder == NULL) {
     lock->holder = current_thread;
@@ -142,6 +112,7 @@ void mutex_lock(struct mutex * lock) {
     yield();
   }
 }
+
 
 void mutex_unlock(struct mutex * lock) {
   // just lelease lock if noone waiting
