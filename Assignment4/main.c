4473eb2f7e8686dbd83702984cb4f95d6e9301f9
Stacy Watts <swatts@pdx.edu>
2014-12-14 03:00:51 -0800

    clean up lines over 80

--- Assignment4/main.c
+++ Assignment4/main.c
@@ -14,35 +14,44 @@ struct mutex lock_one;
 int shared_counter = 0;
 
 
-// I initialy wrote this out and had to debug why the value never got to 16. 
-// at some point I noticed I'd failed to re-check the value of shared_counter.
-// case in point why cached values and multithreading is hard.  
+// I initialy wrote this out and had to debug why the value never got to 16
+// at some point I noticed I'd failed to re-check the value of shared 
+// counter. case in point why cached values and multithreading is hard.  
 void test_mutex_lock(void * arg) {
-   printf("%s  attempts locking, shared counter = %d\n", (char *)arg, shared_counter);
-
+   printf("%s  attempts locking, shared counter = %d\n", 
+         (char *)arg, shared_counter);
    mutex_lock(&lock_one);
    int temp = shared_counter;
-   printf("%s  in critical section, shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  in critical section, shared counter = %d\n", 
+         (char *)arg, shared_counter);
    yield();
-   printf("%s  after yield in critical section shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after yield in critical section shared counter = %d\n", 
+         (char *)arg, shared_counter);
    shared_counter = temp + 1;
-   printf("%s  after update in critical section shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after update in critical section shared counter = %d\n", 
+         (char *)arg, shared_counter);
    yield();
    mutex_unlock(&lock_one);
-   printf("%s  after lock release, shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after lock release, shared counter = %d\n", 
+         (char *)arg, shared_counter);
    yield();
-
-   printf("%s  attempts 2nd locking, shared counter = %d\n", (char *)arg, shared_counter);
+   // second time for locking
+   printf("%s  attempts 2nd locking, shared counter = %d\n", 
+         (char *)arg, shared_counter);
    mutex_lock(&lock_one);
    temp = shared_counter;
-   printf("%s  in 2nd critical section, shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  in 2nd critical section, shared counter = %d\n", 
+         (char *)arg, shared_counter);
    yield();
-   printf("%s  after yield in 2nd critical section shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after yield in 2nd critical section shared counter = %d\n", 
+         (char *)arg, shared_counter);
    shared_counter = temp + 1;
-   printf("%s  after update in 2nd critical section shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after update in 2nd critical section shared counter = %d\n",
+         (char *)arg, shared_counter);
    yield();
    mutex_unlock(&lock_one);
-   printf("%s  after lock release, shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after lock release, shared counter = %d\n", 
+         (char *)arg, shared_counter);
 }
 
 
@@ -56,6 +65,7 @@ void test_threaded_mutex() {
   thread_fork(test_mutex_lock, (void*)"Thread 7");
   thread_fork(test_mutex_lock, (void*)"Thread 8");
 }
+
 
 
 int main(void) {
@@ -77,7 +87,8 @@ void test_unprotected_update(void * arg) {
    int temp = shared_counter;
    printf("%s  starts shared counter = %d\n", (char *)arg, shared_counter);
    yield();
-   printf("%s  after yield in critical section shared counter = %d\n", (char *)arg, shared_counter);
+   printf("%s  after yield in critical section shared counter = %d\n", 
+         (char *)arg, shared_counter);
    shared_counter = temp + 1;
 }
 
